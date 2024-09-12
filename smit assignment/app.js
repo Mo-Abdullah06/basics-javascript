ef0390bf7f7bf0d542bf0fba769e703ac4669518
Muhammad Abdullah <isiabdullah652@gmail.com>
2024-09-12 17:32:07 +0500

    smit assignment

---
+++ smit assignment/app.js
@@ -0,0 +1,31 @@
+
+const firstName = 'abdullah';
+const lastname = 'ali';
+let address = 'Main Street';
+address = 'First Street';
+console.log( firstName +" " + lastname);
+console.log(address);
+
+
+
+
+
+let score1 = 20;
+let score2 = 40;
+let score3 = 60;
+let sum = score1 + score2 + score3;
+let totalScore = sum;
+let averageScore = 40;
+console.log(`Total Score: ${totalScore}`);
+console.log(`Average Score: ${averageScore}`);
+
+
+
+// assignment no: 3
+
+let plates = 20; 
+let people = 7
+let platesPerPerson = plates % people;
+let remainingPlates = platesPerPerson
+let result = `${remainingPlates} plates available`
+console.log(result);
